ee8e773cf73e6a2756b62c7581e67ac8f53fe155
Juliana Campos <julianasoares_85@outlook.com>
2025-05-26 11:59:21 -0300

    meu projeto conta banco

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,12 @@
+public class ContaTerminal {
+
+    int numero=1021;
+    String agencia="067-8";
+    String nomecliente="mario andrade";
+    double saldo=237.48;
+
+    public void escolherAgencia(String novaAgencia){
+        agencia= novaAgencia;
+    }
+
+}
